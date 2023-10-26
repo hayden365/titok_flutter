387455d0ed6b48e8cc09068abd6859af66b630d2
hayden365 <ysmh100@gmail.com>
2023-10-26 21:13:54 +0900

    feat: username_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:titok_flutter/constants/gaps.dart';
+import 'package:titok_flutter/constants/sizes.dart';
+import 'package:titok_flutter/features/authentication/sign_up_screen.dart';
 
 void main() {
   runApp(const TikTokApp());
 }
 
 class TikTokApp extends StatelessWidget {
-  const TikTokApp({super.key});
+  const TikTokApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'TikTok Clone',
-        theme: ThemeData(
-          primaryColor: const Color(0xFFE9435A),
+      title: 'TikTok Clone',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primaryColor: const Color(0xFFE9435A),
+        appBarTheme: const AppBarTheme(
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: Sizes.size16 + Sizes.size2,
+            fontWeight: FontWeight.w600,
+          ),
         ),
-        home: Container());
+      ),
+      home: const SignUpScreen(),
+    );
   }
 }
