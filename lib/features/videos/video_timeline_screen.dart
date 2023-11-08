2948e5983007f9311122d8cb47fcb2b840131675
hayden365 <ysmh100@gmail.com>
2023-11-08 23:48:27 +0900

    feat: page controller

--- lib/features/videos/video_timeline_screen.dart
+++ lib/features/videos/video_timeline_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class VideoTimeLineScreen extends StatefulWidget {
   const VideoTimeLineScreen({super.key});
@@ -8,8 +9,43 @@ class VideoTimeLineScreen extends StatefulWidget {
 }
 
 class _VideoTimelineScreenState extends State<VideoTimeLineScreen> {
+  int _itemCount = 4;
+
+  final PageController _pageController = PageController();
+
+  List<Color> colors = [
+    Colors.blue,
+    Colors.red,
+    Colors.yellow,
+    Colors.teal,
+  ];
+
+  void _onPageChanged(int page) {
+    if (page == _itemCount - 1) {
+      _pageController.animateTo(0,
+          duration: const Duration(seconds: 5), curve: Curves.bounceOut);
+      _itemCount = _itemCount + 4;
+      colors.addAll(colors);
+      setState(() {});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return PageView.builder(
+      controller: _pageController,
+      scrollDirection: Axis.vertical,
+      onPageChanged: _onPageChanged,
+      itemCount: _itemCount,
+      itemBuilder: (context, index) => Container(
+        color: colors[index],
+        child: Center(
+          child: Text(
+            "Screen $index",
+            style: const TextStyle(fontSize: 68),
+          ),
+        ),
+      ),
+    );
   }
 }
