5a571a55f106b52f8ac9180a305a1af0f013650b
hayden365 <ysmh100@gmail.com>
2023-11-13 23:38:41 +0900

    feat: video timeline

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:titok_flutter/constants/gaps.dart';
 import 'package:titok_flutter/constants/sizes.dart';
+import 'package:titok_flutter/features/videos/widgets/video_button.dart';
 import 'package:video_player/video_player.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
@@ -20,7 +22,7 @@ class VideoPost extends StatefulWidget {
 
 class _VideoPostState extends State<VideoPost>
     with SingleTickerProviderStateMixin {
-  final VideoPlayerController _videoPlayerController =
+  VideoPlayerController _videoPlayerController =
       VideoPlayerController.asset("assets/videos/video.mp4");
   final Duration _animationDuration = const Duration(milliseconds: 200);
 
@@ -38,10 +40,12 @@ class _VideoPostState extends State<VideoPost>
   }
 
   void _initVideoPlayer() async {
+    _videoPlayerController =
+        VideoPlayerController.asset("assets/videos/video.mp4");
     await _videoPlayerController.initialize();
-
-    setState(() {});
+    await _videoPlayerController.setLooping(true);
     _videoPlayerController.addListener(_onVideoChange);
+    setState(() {});
   }
 
   @override
@@ -130,7 +134,62 @@ class _VideoPostState extends State<VideoPost>
                   ),
                 ),
               ),
-            )
+            ),
+            const Positioned(
+              bottom: 20,
+              left: 10,
+              child: Column(
+                children: [
+                  Text(
+                    '@니꼬',
+                    style: TextStyle(
+                      fontSize: Sizes.size20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Gaps.v10,
+                  Text(
+                    "Focuspang",
+                    style: TextStyle(
+                      fontSize: Sizes.size16,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const Positioned(
+              bottom: 20,
+              right: 10,
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    radius: 25,
+                    backgroundColor: Colors.black,
+                    foregroundColor: Colors.white,
+                    foregroundImage: NetworkImage(
+                        "https://avatars.githubusercontent.com/u/109419531?v=4"),
+                    child: Text("니꼬"),
+                  ),
+                  Gaps.v24,
+                  VideoButton(
+                    icon: FontAwesomeIcons.solidHeart,
+                    text: "2.9M",
+                  ),
+                  Gaps.v24,
+                  VideoButton(
+                    icon: FontAwesomeIcons.solidComment,
+                    text: "33K",
+                  ),
+                  Gaps.v24,
+                  VideoButton(
+                    icon: FontAwesomeIcons.share,
+                    text: "Share",
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
